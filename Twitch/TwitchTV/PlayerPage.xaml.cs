fedbcb79d3875e14962cc90d32ece67a50a14280
Ryan Price <rwprice01@gmail.com>
2013-11-17 15:38:38 -0500

    Added New Package for toolkit use Almost have quality selection working

--- Twitch/TwitchTV/PlayerPage.xaml.cs
+++ Twitch/TwitchTV/PlayerPage.xaml.cs
@@ -9,6 +9,8 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using TwitchAPIHandler.Objects;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace TwitchTV
 {
@@ -17,24 +19,56 @@ namespace TwitchTV
         public AccessToken token { get; set; }
         public M3U8Playlist playlist { get; set; }
 
+        public List<string> qualitys { get; set; }
+
+        public string quality { get; set; }
+
         public PlayerPage()
         {
+            qualitys = new List<string>();
+            token = new AccessToken();
+            playlist = new M3U8Playlist();
             InitializeComponent();
         }
 
-        public async void GetVideo()
+        public async void GetQualities()
         {
-            //Go to the video player page
-            token = await AccessToken.GetToken(App.ViewModel.channel);
-            playlist = await M3U8Playlist.GetStreamPlaylist(App.ViewModel.channel, token);
+            if (string.IsNullOrEmpty(token.Token) || string.IsNullOrEmpty(token.Signature))
+            {
+                //Go to the video player page
+                token = await AccessToken.GetToken(App.ViewModel.channel);
+                playlist = await M3U8Playlist.GetStreamPlaylist(App.ViewModel.channel, token);
+            }
 
-            string path_to_video = playlist.streams["Low"];
+            foreach (var qual in playlist.streams.Keys)
+                qualitys.Add(qual);
+
+            if(quality == "" || quality == null)
+                quality = qualitys[qualitys.Count-1];
+
+            qualitys.Reverse();
+
+            this.QualitySelection.ItemsSource = qualitys;
+            this.QualitySelection.SelectedItem = quality;
+        }
+
+        public void GetVideo()
+        {
+            this.Video.Close();
+
+            string path_to_video = playlist.streams[quality];
+
+            this.Video.ControlPanel.IsEnabled = false;
+            this.Video.ControlPanel.Visibility = System.Windows.Visibility.Collapsed;
+
             this.Video.Source = new Uri(path_to_video, UriKind.RelativeOrAbsolute);
+            this.Video.Play();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            GetVideo();
+            GetQualities();
+            base.OnNavigatedTo(e);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
@@ -46,6 +80,30 @@ namespace TwitchTV
         protected override void OnOrientationChanged(OrientationChangedEventArgs e)
         {
             base.OnOrientationChanged(e);
+        }
+
+        private void QualitySelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var obj = ((ListPicker)(sender)).SelectedItem;
+            if (obj != null)
+            {
+                quality = (string)obj;
+                GetVideo();
+            }
+        }
+
+        private void Video_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (this.TaskBar.Opacity == 0)
+            {
+                this.TaskBar.Opacity = 1;
+                this.QualitySelection.Opacity = 1;
+            }
+            else
+            {
+                this.TaskBar.Opacity = 0;
+                this.QualitySelection.Opacity = 0;
+            }
         }
     }
 }
